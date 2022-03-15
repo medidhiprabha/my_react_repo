2619e1540ec3f036519676ba223e43b0b66f9306
medidhiprabha <medidhiprabha1997@gmail.com>
2022-03-16 00:51:36 +0530

    my second commit

--- mysampleapp/src/App.js
+++ mysampleapp/src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
        <p>my first app</p>
+       <h2>hey this is my second commit</h2>
       </header>
     </div>
   );
